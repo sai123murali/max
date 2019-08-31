b8026e1f1b576e1a4038acab2323c0cd91d0fa71
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2019-08-31 11:16:59 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+bool arePara(string expr) 
+{ 
+    stack<char> s; 
+    char x; 
+
+    for (int i=0; i<expr.length(); i++) 
+    { 
+        if (expr[i]=='('||expr[i]=='['||expr[i]=='{') 
+        { 
+            s.push(expr[i]); 
+            continue; 
+        } 
+
+        if (s.empty()) 
+           return false; 
+  
+        switch (expr[i]) 
+        { 
+        case ')': 
+            x = s.top(); 
+            s.pop(); 
+            if (x=='{' || x=='[') 
+                return false; 
+            break; 
+  
+        case '}': 
+  
+            x = s.top(); 
+            s.pop(); 
+            if (x=='(' || x=='[') 
+                return false; 
+            break; 
+  
+        case ']': 
+
+            x = s.top(); 
+            s.pop(); 
+            if (x =='(' || x == '{') 
+                return false; 
+            break; 
+        } 
+    } 
+
+    return (s.empty()); 
+} 
+
+int main() 
+{ 
+    string expr; 
+    cin>>expr;
+  
+    if (arePara(expr)) 
+        cout << "YES"; 
+    else
+        cout << "NO"; 
+    return 0; 
+}
